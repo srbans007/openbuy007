85042a8406e8f463f567d7ae79252558da11e0cb
srbans007 <sr_bans007@hotmail.com>
2023-08-02 20:55:12 -0400

    read table

--- src/app/components/dashboard/home/todo-carga/todo-carga.component.ts
+++ src/app/components/dashboard/home/todo-carga/todo-carga.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Todo_carga } from 'src/app/interfaces/todo-carga';
+import { TodoCargaService } from 'src/app/services/todo-carga.service';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-carga',
@@ -6,5 +11,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-carga.component.scss']
 })
 export class TodoCargaComponent {
+  listGuias: Todo_carga[] = []
 
+  constructor(
+    private router: Router,
+    private _TodoCargaService: TodoCargaService
+
+  ) { }
+
+  ngOnInit(): void {
+    this.getGuias();
+  }
+
+  getGuias() {
+    this._TodoCargaService.getTodoCarga()
+      .pipe(
+        catchError(error => {
+          if (error.status === 400) {
+            // Aquí debes manejar tu lógica de logout, por ejemplo:
+            this.logOut(); // Asegúrate de tener definida esta función
+            console.log("aqui deberia mandarlo a la mierde")
+          }
+          throw error;
+        })
+      )
+      .subscribe(data => {
+          this.listGuias = data;
+      }, error => {
+          console.error('Ocurrió un error:', error);
+      });
+  }
+
+
+  logOut() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login'])
+  }
 }
--- src/app/services/todo-carga.service.ts
+++ src/app/services/todo-carga.service.ts
@@ -8,6 +8,18 @@ import { Todo_carga } from '../interfaces/todo-carga';
   providedIn: 'root'
 })
 export class TodoCargaService {
+  private myAppUrl: string;
+  private myApiUrl2: string;
 
-  
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl2 = 'api/troncal'
+  }
+
+  getTodoCarga(): Observable<Todo_carga[]> {
+    const token = localStorage.getItem('token')
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
+    return this.http.get<Todo_carga[]>(`${this.myAppUrl}${this.myApiUrl2}`, { headers: headers })
+    //return this.http.get<Todo_carga[]>(`${this.myAppUrl}${this.myApiUrl2}`)
+  }
 }
