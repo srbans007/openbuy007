754fb487b007ef9d0043d5c3dc6e0c2d51e74d94
srbans007 <sr_bans007@hotmail.com>
2023-08-15 04:43:55 -0400

    raiz ruta

---
+++ src/app/interfaces/ruta.ts
@@ -0,0 +1,9 @@
+export interface Ruta {
+    id?: number,
+    id_sucursal?: number,
+    id_chofer?: number,
+    id_ayudante?: number,
+    id_vehiculo?: number,
+    id_tipoRuta?: number,
+    id_tim?: number
+}
---
+++ src/app/services/ruta.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Ruta } from '../interfaces/ruta';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RutaService {
+  private myAppUrl: string;
+  private listar: string;
+  private insertar: string;
+  private eliminar: string;
+  private update: string;
+  private headers: HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = `${environment.endpoint}api/ruta`;
+    this.listar = ''
+    this.insertar = '/insert'
+    this.eliminar = '/eliminar'
+    this.update = '/update'
+
+    this.headers = this.createHeaders();
+  }
+
+  private createHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  getRuta(): Observable<Ruta[]> {
+    return this.http.get<Ruta[]>(`${this.myAppUrl}${this.listar}`, { headers: this.headers });
+  }
+
+  insertRuta(ruta: Ruta[]): Observable<Ruta[]> {
+    return this.http.post<Ruta[]>(`${this.myAppUrl}${this.insertar}`, ruta, { headers: this.headers });
+  }
+
+  updateRuta(ruta: Ruta[]): Observable<Ruta[]> {
+    return this.http.post<Ruta[]>(`${this.myAppUrl}${this.update}`, ruta, { headers: this.headers });
+  }
+
+  destroyRuta(ruta: Ruta[]): Observable<Ruta[]> {
+    return this.http.post<Ruta[]>(`${this.myAppUrl}${this.eliminar}`, ruta, { headers: this.headers });
+  }
+}
